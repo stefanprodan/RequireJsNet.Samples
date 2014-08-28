94a65803ec9d1e1456ba142d8fae7e705faf2467
Stefan Prodan <stefan.prodan@gmail.com>
2014-08-28 22:13:40 +0300

    one bundle per MVC Area (compressor setup)

--- RequireJsNet.ProjectTemplate/Scripts/Controllers/Admin/Dashboard/Index.js
+++ RequireJsNet.ProjectTemplate/Scripts/Controllers/Admin/Dashboard/Index.js
@@ -1,1 +1,24 @@
-﻿
+﻿require([
+        'i18n!nls/app-resx',
+        'jquery',
+        'jquery-validate',
+        'jquery-validate-unobtrusive',
+        'bootstrap',
+        'respond'
+], function (resx) {
+
+    var ctrl = function () {
+
+        this.init();
+    };
+
+    ctrl.prototype.init = function () {
+
+        //init
+    };
+
+    //create object on DOM ready
+    $(function () {
+        var entryScript = new ctrl();
+    });
+});
